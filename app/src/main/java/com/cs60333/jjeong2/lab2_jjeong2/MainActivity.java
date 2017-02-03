70cbdfea30df9c7c0086cb7bd66a63b8a6a917bc
Ji Hwan Jeong <Ji Hwan Jeong>
2017-02-03 14:59:11 -0500

    Initial commit

---
+++ app/src/main/java/com/cs60333/jjeong2/lab2_jjeong2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cs60333.jjeong2.lab2_jjeong2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
